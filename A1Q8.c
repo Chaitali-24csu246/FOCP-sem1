e88305d4167b6cbaec9003eebc23d4ba920a5095
Chaitali <24csu246@ncuindia.edu>
2024-11-07 20:03:44 +0530

    Create A1Q8.c

---
+++ A1Q8.c
@@ -0,0 +1,18 @@
+//ASSIGNMENT 1 Q8 PATTERN PRINTING
+#include <stdio.h>
+int main(){
+    int i,j;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=i;j++){
+           if((i-j)%2!=0){
+               printf("0");
+           }
+           else{
+               printf("1");
+           }
+            
+        }
+        printf("\n");
+    }
+    return 0;
+}
