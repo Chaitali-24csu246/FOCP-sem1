9c6690d528974cc7b893a14d84ca3877ffc5dca8
Chaitali <24csu246@ncuindia.edu>
2024-11-07 20:53:22 +0530

    Create A2Q10.c

---
+++ A2Q10.c
@@ -0,0 +1,33 @@
+//ASSIGNMENT 2 Q10 COUNT PRIME NUMBERS IN AN ARRAY
+#include <stdio.h>
+//COUNT PRIME NUMBERS IN AN ARRAY
+int takearray(int arr[], int n){
+    int i;
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+}
+void prime(int elem){
+    int a=0,i;
+    for(i=2;i<elem;i++){
+        if(elem%i==0)
+        a++;
+    }
+    if(a!=0){
+        printf("%d is not a prime number\n",elem);
+    } else {
+        printf("%d is a prime number",elem);
+    }
+}
+int main(){
+    int n;
+    printf("Enter number of elements you want to enter ");
+    scanf("%d",&n);
+    int arr[n];
+    takearray(arr,n);
+    int i;
+    for(i=0;i<n;i++){
+        prime(arr[i]);
+    }
+    return 0;
+}
