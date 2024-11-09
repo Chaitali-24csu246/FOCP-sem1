548ea8f085d3bf305301e97e56fdcf07c76cc2dc
Chaitali <24csu246@ncuindia.edu>
2024-11-09 15:29:40 +0530

    Update A2Q11.c

--- A2Q11.c
+++ A2Q11.c
@@ -1,42 +1,27 @@
+// INSERT AT ANY POSITION
 #include <stdio.h>
-//ARRAY INSERTION
 int main() {
-    int s, i,p,v;
-    printf("Enter size: ");
-    scanf("%d", &s);
-    int a[s];
-    printf("Enter elements:\n");
-    for(i= 0; i<s;i++) {
-        scanf("%d", &a[i]);
+    int l[11]={1,2,3,4,5,6,7,8,9,10};
+    int len=sizeof(l)/sizeof(l[0]);
+    int i,ind,num;
+    printf("List is: ");
+    for(i=0;i<len-1;i++){
+        printf("%d ",l[i]);
     }
-    printf("Original array: ");
-    for (i = 0; i < s; i++) {
-        printf("%d ", a[i]);
-    }
-    printf("\nposition: ");
-    scanf("%d", &p);
-    if(p>s+1 || p<1) {
-        printf("Invalid position\n");
-    }else{
-        printf("Enter value: ");
-        scanf("%d", &v);
-        if(p==1) {
-            for(i=s;i>=1;i--){
-                a[i]=a[i-1];}
-            a[0]=v;
-            s++;
-        }else if(p==s+1){
-            a[s-1]=v;
-            s++;
-        }else{
-            for (i=s;i>=p; i--) {
-                a[i]=a[i-1];}
-            a[p-1]= v;
-            s++;
+    printf("\nEnter position (1 to %d) at which you want to insert: ",len);
+    scanf("%d",&ind);
+    if(ind<=len && ind>0){
+    for(i=len-1;i>=ind-1;i--){
+            l[i]=l[i-1];
         }
-        printf("Array after insertion: ");
-        for (i=0; i<s; i++) {
-            printf("%d ", a[i]);
-        }}
+        printf("Enter number to be inserted at that position");
+        scanf("%d",&num);
+        l[ind-1]=num;
+        for(i=0;i<len;i++){
+        printf("%d ",l[i]);
+    }
+    } else {
+        printf("Position not in range");
+    }
     return 0;
 }
