a19141e8f1a1f38d34cb42309ecfbd12cafffa31
Chaitali <24csu246@ncuindia.edu>
2024-11-11 20:40:21 +0530

    Update A5Hangman.c

--- A5Hangman.c
+++ A5Hangman.c
@@ -1,116 +1,123 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>//using for uppercase and lowercase consistency
-void printInstructions();
-void displayWord(char word[], char guess[], int wordLength);
-int isLetterInWord(char letter, char word[], int wordLength);
-void toLowerCase(char word[], int length);
-int main() {
-    char word[100],hint[100],  guessWord[100];//giving a maximum length to reduce complexity
-    char guess;
-    int tries=10, wordLength;
-    int gameOver=0;
-    printInstructions();
-
-    printf("Player 1, enter a word: ");
-    gets(word, sizeof(word), stdin);
-    word[strcspn(word, "\n")] = 0;  // Removing newline character as it caused errors
-
-    printf("Player 1, enter a hint: ");
-    gets(hint, sizeof(hint), stdin);
-    hint[strcspn(hint, "\n")] = 0;  // rmove newline 
-    // lowercase uppercase
-    toLowerCase(word, strlen(word));
-    // Creating guessword
-    wordLength=strlen(word);
-    for (int i=0;i<wordLength;i++) {
-        if (strchr("aeiou", word[i])) {
-            guessWord[i] = word[i];  // show vowels
+#include <ctype.h>
+void tlc(char word[], int length) {
+    int i;
+    for (i = 0; i < length; i++) {
+        word[i] = tolower(word[i]);
+    }
+}
+int display(char word[], int len, int nums[]) {
+    int i, n = 0;
+    for (i = 0; i < len; i++) {
+        if (word[i] == 'a' || word[i] == 'e' || word[i] == 'o' || word[i] == 'i' || word[i] == 'u' || nums[i] == 1) {
+            printf("%c", word[i]);
+            n++;
         } else {
-            guessWord[i] = '_';  // Hide consonants
+            printf("_ ");
         }
     }
-    guessWord[wordLength] = '\0';  //terminate the guess word with null
+    printf("\n");
+    return n;
+}
 
-    // Game loop
-    while (!gameOver && tries > 0) {
-        printf("\nCurrent word: ");
-        displayWord(word, guessWord, wordLength); 
-
-        printf("\nPlayer 2, you have %d tries left.\n", tries);
-        printf("Do you want a hint (h), guess a letter (l), or guess the word (w)? ");
-        char choice;
-        scanf(" %c", &choice);  
-        choice = tolower(choice);  // Convert choice to lowercase for error handling
-        if (choice == 'h') {
-            // Hint reduces one try
-        printf("Hint: %s\n", hint);
-        tries--;  
-        } else if (choice == 'l') {
-            printf("Enter a letter: ");
-            scanf(" %c", &guess);
-            guess = tolower(guess);  
-            // Check if letter is in the word
-            int found = 0;
-            for (int i = 0; i < wordLength; i++) {
-                if (word[i] == guess) {
-                    guessWord[i] = guess;  // Update guessWord 
-                    found = 1;
+int allLettersGuessed(int nums[], int len) {
+    for (int i = 0; i < len; i++) {
+        if (nums[i] == 0) {
+            return 0;
+        }
     }
-            }
-            if (found) {
-                printf("Correct guess!\n");
+    return 1;
+}
+
+void game(char word[], int len, char hint[], int nums[]) {
+    int i, choice, y, flag = 1, p = 0;
+    char guess[len + 1], w;
+
+    for (i = 1; i <= 10 && flag == 1; i++) {
+        printf("Attempt number %d\n", i);
+        printf("Choose an option:\n 1) Guess word\n 2) Get hint\n 3) Guess a letter\n");
+        scanf("%d", &choice);
+        getchar();
+
+        if (choice == 1) { 
+            printf("Enter your guess: ");
+            fgets(guess, len + 1, stdin);
+            guess[strcspn(guess, "\n")] = '\0';
+
+            if (strcmp(word, guess) == 0) {
+                printf("Correct guess, you win!\n");
+                flag = 0;
+                break;
             } else {
-                printf("Wrong guess!\n");
-            }
-            // Deduct one try 
-            tries--; }  else if(choice=='w'){
-            //  guesses the whole word
-            printf("Guess the word: ");
-            char playerGuess[100];
-            scanf("%s", playerGuess);
-            toLowerCase(playerGuess, strlen(playerGuess));
-            if (strcmp(playerGuess, word) == 0) {
-                printf("Correct! Player 2 wins!\n");
-                gameOver = 1;  // End the game 
-                printf("Wrong guess! Try again.\n");
+                printf("Incorrect guess.\n");
             }
 
-            tries--;
-        } else {
-            printf("Invalid choice! Please choose 'h', 'l', or 'w'.\n");}
-        if (strcmp(word, guessWord) == 0) {
-            printf("Congratulations! Player 2 guessed the word correctly.\n");
-            gameOver = 1;}}
-    if (!gameOver) {//!ameover=gameover!=1 false codition
-        printf("Player 1 wins! Player 2 ran out of tries.\n");
+        } else if (choice == 2) { 
+            printf("Hint: %s\n", hint);
+
+        } else if (choice == 3) { 
+            printf("Guess a letter: ");
+            scanf(" %c", &w);
+            w = tolower(w);
+            int found = 0;
+
+            for (y = 0; y < len; y++) {
+                if (word[y] == w) {
+                    nums[y] = 1;
+                    found = 1;
+                }
+            }
+            if (found == 1) {
+                printf("Correctly guessed letter!\n");
+                if (display(word, len, nums) == len) {
+                    printf("Game won. You guessed it!\n");
+                    flag = 0;
+                    break;
+                }
+            } else {
+                printf("Letter not in word.\n");
+            }
+        } else { 
+            printf("Invalid input, please choose 1, 2, or 3.\n");
+            i--;
+            continue;
+        }
+
+        display(word, len, nums);
+
+        if (allLettersGuessed(nums, len)) {
+            printf("Congratulations, you've guessed the word!\n");
+            flag = 0;
+            break;
+        }
     }
+
+    if (flag == 1) {
+        printf("Out of attempts! The word was: %s\n", word);
+    }
+}
+
+int main() {
+    int len;
+    printf("Welcome to Hangman!\nPlayer 1 provides a word and a hint.\nPlayer will try to guess the word.\nOnly vowels are shown initially.\nPlayer 2 can guess letters, the word, or ask for a hint.\nNote: Do not include spaces or newlines in the word\nTry to enter all values in lowercase as upper case occasionally gives error");
+
+    printf("\nPlayer 1, enter the number of characters in the word: ");
+    scanf("%d", &len); getchar();
+    char word[len + 1], hint[100];
+    int nums[len];
+    memset(nums, 0, sizeof(nums));  
+    printf("Enter the word: ");
+    fgets(word, len + 1, stdin);
+    word[strcspn(word, "\n")] = '\0';
+    printf("Enter a hint: "); getchar();
+    fgets(hint, sizeof(hint), stdin);
+    hint[strcspn(hint, "\n")] = '\0';
+    tlc(word, len);
+    printf("Now it's Player 2's turn.\n");
+    display(word, len, nums);
+    game(word, len, hint, nums);
+
     return 0;
 }
-void printInstructions() {
-    printf("Welcome to Hangman!\n");
-    printf("Player 1 will provide a word and a hint. \nplayer 2 will try to guess the word.\n");
-    printf("The word will be partially displayed \n Plyer 2 can guess a letter, a word, or ask for a hint.\n");
-    printf("You have 10 tries to guess the word correctly.\n If Player  two uses a hint, one try is deductd.\n");
-    printf("Good luck!\n");
-}
-// Function to display the current state 
-void displayWord (char word[],char guess[], int wordLength){
-    for(int i=0;i<wordLength;i++){
-        if (guess[i] == '_') {
-            printf("_ ");
-        } else {
-printf("%c ", guess[i]);}}
-    printf("\n");}
 
-// Function to check if a guessed letter is in the word
-int isLetterInWord(char letter, char word[], int wordLength) {
-    for (int i = 0; i < wordLength; i++) {
-        if (word[i] == letter) {
-            return 1;  // Letter found 
-            }}
-    return 0;  // Letter not found
-}// convert a string to lowercase
-void toLowerCase(char word[], int length) {
-for (int i=0;i<length; i++) {
-        word[i] = tolower(word[i]);}}
